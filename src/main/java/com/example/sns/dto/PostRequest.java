2b41dc79a10b4069bc9dc9db50ba079677cfa4f1
geunhokinn <gghh1220@gmail.com>
2024-10-28 23:22:25 +0900

    Feat: PostRequest 작성 - 글 작성을 요청을 받기 위한 CreateDTO 작성

---
+++ src/main/java/com/example/sns/dto/PostRequest.java
@@ -0,0 +1,23 @@
+package com.example.sns.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class PostRequest {
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class CreateDTO {
+
+        private String title;
+        private String body;
+
+        @Builder
+        public CreateDTO(String title, String body) {
+            this.title = title;
+            this.body = body;
+        }
+    }
+}
