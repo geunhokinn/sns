69cbe6097ac22e325480d9a2a304be8dad030a87
geunhokinn <gghh1220@gmail.com>
2024-10-28 23:19:49 +0900

    Feat: Post 작성 - Post 엔티티 작성 - User 엔티티와 N:1 연관관계 설정 - 연관관계 편의 메서드 assignUser() 추가

---
+++ src/main/java/com/example/sns/entity/Post.java
@@ -0,0 +1,78 @@
+package com.example.sns.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE user SET deleted_at = NOW() where id = ?")
+@Where(clause = "deleted_at is NULL")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    @Builder
+    private Post(String title, String body, User user) {
+        this.title = title;
+        this.body = body;
+        this.assignUser(user);
+    }
+
+    public static Post of(String title, String body, User user) {
+        return Post.builder()
+                .title(title)
+                .body(body)
+                .user(user)
+                .build();
+    }
+
+    // Post N <-> 1 User
+    // 양방향 연관관계 편의 메서드
+    public void assignUser(User user) {
+        if(this.getUser() != null) {
+            this.user.getPosts().remove(this);
+        }
+
+        this.user = user;
+
+        if(!user.getPosts().contains(this)) {
+            user.addPost(this);
+        }
+    }
+}
