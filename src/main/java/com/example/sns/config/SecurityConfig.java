667f27d0563381c58f2209e7feaed806a76150c0
geunhokinn <gghh1220@gmail.com>
2024-10-28 23:06:39 +0900

    Feat: SecurityConfig 작성 - JWTFilter 추가 - CustomAuthenticationEntryPoint 추가

--- src/main/java/com/example/sns/config/SecurityConfig.java
+++ src/main/java/com/example/sns/config/SecurityConfig.java
@@ -1,14 +1,36 @@
 package com.example.sns.config;
 
+import com.example.sns.exception.auth.CustomAuthenticationEntryPoint;
+import com.example.sns.filter.JWTFilter;
+//import com.example.sns.filter.LoginFilter;
+import com.example.sns.util.JWTUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+//    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    private final JWTUtil jwtUtil;
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -26,9 +48,25 @@ public class SecurityConfig {
                         .requestMatchers("/api/*/users/login", "/api/*/users/join").permitAll()
                         .anyRequest().authenticated());
 
+//        http
+//                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+//
+//        LoginFilter loginFilter = new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil);
+//        loginFilter.setFilterProcessesUrl("/api/*/users/login");
+//
+//        http
+//                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPoint));
 
         return http.build();
     }
