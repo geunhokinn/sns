6dc46e5c6241c0042d10be81316be9a923354822
geunhokinn <gghh1220@gmail.com>
2024-10-28 23:05:54 +0900

    Feat: CustomAuthenticationEntryPoint 작성 - 인증 실패시 403 에러 대신 커스텀한 에러를 던져주기 위해 CustomAuthenticationEntryPoint 작성

---
+++ src/main/java/com/example/sns/exception/auth/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.example.sns.exception.auth;
+
+import com.example.sns.enumerate.ErrorCode;
+import com.example.sns.response.Response;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(ErrorCode.INVALID_TOKEN.getStatus().value());
+        response.getWriter().write(Response.error(ErrorCode.INVALID_TOKEN.name()).toStream());
+    }
+}
