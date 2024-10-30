29fd3f7a4d0766d486db4c0002d6e52ec782387f
geunhokinn <gghh1220@gmail.com>
2024-10-30 22:24:41 +0900

    Feat: PostController. PostService 작성 - 글 수정을 위한 updatePost() 작성

--- src/main/java/com/example/sns/controller/PostController.java
+++ src/main/java/com/example/sns/controller/PostController.java
@@ -1,15 +1,13 @@
 package com.example.sns.controller;
 
 import com.example.sns.dto.PostRequest;
+import com.example.sns.dto.PostResponse;
 import com.example.sns.response.Response;
 import com.example.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -25,5 +23,13 @@ public class PostController {
         postService.createPost(createDTO.getTitle(), createDTO.getBody(), authentication.getName());
 
         return Response.success();
+    }
+
+    @PutMapping("/{postId}")
+    public Response<PostResponse.UpdateDTO> updatePost(@PathVariable Long postId, @RequestBody PostRequest.UpdateDTO updateDTO, Authentication authentication) {
+
+        PostResponse.UpdateDTO updateResponse = postService.updatePost(updateDTO.getTitle(), updateDTO.getBody(), authentication.getName(), postId);
+
+        return Response.success(updateResponse);
     }
 }
--- src/main/java/com/example/sns/service/PostService.java
+++ src/main/java/com/example/sns/service/PostService.java
@@ -1,5 +1,6 @@
 package com.example.sns.service;
 
+import com.example.sns.dto.PostResponse;
 import com.example.sns.entity.Post;
 import com.example.sns.entity.User;
 import com.example.sns.enumerate.ErrorCode;
@@ -25,5 +26,23 @@ public class PostService {
                 .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username)));
 
         postRepository.save(Post.of(title, body, user));
+    }
+
+    @Transactional
+    public PostResponse.UpdateDTO updatePost(String title, String body, String username, Long postId) {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username)));
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+
+        if (post.getUser() != user) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("s has no permission with %s", username, postId));
+        }
+
+        post.update(title, body);
+
+        return PostResponse.UpdateDTO.from(postRepository.saveAndFlush(post));
     }
 }
