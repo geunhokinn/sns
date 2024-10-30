305a8afecd5878107bd88c74e7e8810ba13e0014
geunhokinn <gghh1220@gmail.com>
2024-10-30 22:20:06 +0900

    Feat: ErrorCode 작성 - Post 를 찾을 수 없는 상황에서 404 에러를 반환하기 위해 POST_NOT_FOUND 추가 - 자신이 아닌 다른 유저가 Post 를 수정하는 상황에서 401 에러를 반환하기 위해 INVALID_PERMISSION 추가

--- src/main/java/com/example/sns/enumerate/ErrorCode.java
+++ src/main/java/com/example/sns/enumerate/ErrorCode.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "Username is duplicated"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     ;
