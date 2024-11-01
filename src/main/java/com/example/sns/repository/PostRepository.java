1bd21b54df5e45ee774de7c9d67e31db2ec2ef38
geunhokinn <gghh1220@gmail.com>
2024-11-01 18:19:53 +0900

    Feat: PostRepository 작성 - 유저 개인에 대한 게시물들을 조회 하기 위해 findAllByUser() 작성

--- src/main/java/com/example/sns/repository/PostRepository.java
+++ src/main/java/com/example/sns/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package com.example.sns.repository;
 
 import com.example.sns.entity.Post;
+import com.example.sns.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findAllByUser(User user, Pageable pageable);
 }
