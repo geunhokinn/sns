d2242f5977c8f7612257c5cf8e40b12822d6433c
geunhokinn <gghh1220@gmail.com>
2024-11-06 17:59:02 +0900

    Feat: CommentRepository 작성 - n+1 문제를 해결하고 soft delete 게시물 전에 댓글 삭제하기 위한 deleteAllByPost() 작성

--- src/main/java/com/example/sns/repository/CommentRepository.java
+++ src/main/java/com/example/sns/repository/CommentRepository.java
@@ -5,8 +5,18 @@ import com.example.sns.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Page<Comment> findAllByPost(Post post, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE Comment c SET c.deletedAt = NOW() where c.post = :post")
+    void deleteAllByPost(@Param("post") Post post);
+
+//    void deleteAllByPost(Post post);
+
 }
