25a2e22b1abf73578d0a3d56ce8151f448ecf4c4
geunhokinn <gghh1220@gmail.com>
2024-11-06 17:58:44 +0900

    Feat: LikeEntityRepository 작성 - n+1 문제를 해결하고 soft delete 게시물 전에 좋아요를 삭제하기 위한 deleteAllByPost() 작성

--- src/main/java/com/example/sns/repository/LikeEntityRepository.java
+++ src/main/java/com/example/sns/repository/LikeEntityRepository.java
@@ -4,6 +4,9 @@ import com.example.sns.entity.LikeEntity;
 import com.example.sns.entity.Post;
 import com.example.sns.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,4 +15,10 @@ public interface LikeEntityRepository extends JpaRepository<LikeEntity, Long> {
     Optional<LikeEntity> findByUserAndPost(User user, Post post);
 
     Long countByPost(Post post);
+
+    @Modifying
+    @Query("UPDATE LikeEntity l SET l.deletedAt = NOW() where l.post = :post")
+    void deleteAllByPost(@Param("post") Post post);
+
+//    void deleteAllByPost(Post post);
 }
