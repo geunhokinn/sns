9caf32cfb9c13094657a47c356451b67c3623028
geunhokinn <gghh1220@gmail.com>
2024-11-09 15:56:55 +0900

    Feat: InfoDTO 작성 - 레디스에 유저 정보를 저장하기 위한 InfoDTO 작성

--- src/main/java/com/example/sns/dto/auth/InfoDTO.java
+++ src/main/java/com/example/sns/dto/auth/InfoDTO.java
@@ -1,22 +1,45 @@
 package com.example.sns.dto.auth;
 
+import com.example.sns.entity.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InfoDTO {
 
+    private Long id;
     private String username;
     private String password;
     private String role;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
 
     @Builder
-    public InfoDTO(String username, String password, String role) {
+    private InfoDTO(Long id, String username, String password, String role, Timestamp registeredAt, Timestamp updatedAt, Timestamp deletedAt) {
+        this.id = id;
         this.username = username;
         this.password = password;
         this.role = role;
+        this.registeredAt = registeredAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public static InfoDTO from(User user) {
+        return InfoDTO.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .role(user.getRole())
+                .registeredAt(user.getRegisteredAt())
+                .updatedAt(user.getUpdatedAt())
+                .deletedAt(user.getDeletedAt())
+                .build();
     }
 }
