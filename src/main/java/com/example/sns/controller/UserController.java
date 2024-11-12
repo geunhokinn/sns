f2069be6075cf958ee1eb0fe224b609362498f2a
geunhokinn <gghh1220@gmail.com>
2024-11-12 16:13:55 +0900

    Feat: UserController 작성 - 알람 구독 컨트롤러 작성

--- src/main/java/com/example/sns/controller/UserController.java
+++ src/main/java/com/example/sns/controller/UserController.java
@@ -4,12 +4,14 @@ import com.example.sns.dto.AlarmResponse;
 import com.example.sns.response.Response;
 import com.example.sns.dto.UserRequest;
 import com.example.sns.dto.UserResponse;
+import com.example.sns.service.AlarmService;
 import com.example.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AlarmService alarmService;
 
     @PostMapping("/join")
     public Response<UserResponse.JoinDTO> join(@RequestBody UserRequest.JoinDTO joinDTO) {
@@ -34,5 +37,11 @@ public class UserController {
     public Response<Page<AlarmResponse.ReadDTO>> getAlarmList(Pageable pageable, Authentication authentication) {
 
         return Response.success(userService.getAlarmList(pageable, authentication.getName()));
+    }
+
+    @GetMapping("/alarm/subscribe")
+    public SseEmitter subscribe(Authentication authentication) {
+
+        return alarmService.connectAlarm(authentication.getName());
     }
 }
